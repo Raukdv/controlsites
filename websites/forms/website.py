7c0056577801d9a9791fdd65723159763c7aa878
Raul Diaz <raukateromadness@gmail.com>
2024-05-23 14:51:01 -0400

    removing unused spaces

--- websites/forms/website.py
+++ websites/forms/website.py
@@ -1,14 +1,15 @@
 from django import forms
 
 from websites.models.website import Website
-
+from websites.tools.checking import is_valid_hostname
+from django.core.exceptions import ValidationError
 
 class WebsitesForm(forms.ModelForm):
 
     domain = forms.CharField(required=True, label='domain', 
             widget = forms.TextInput(attrs={
                 'class':'form-control',
-                'placeholder': 'domain',
+                'placeholder':'domain',
             }
         )
     )
@@ -16,7 +17,7 @@ class WebsitesForm(forms.ModelForm):
     user = forms.CharField(required=False, label='user', 
             widget = forms.TextInput(attrs={
                 'class':'form-control', 
-                'placeholder': 'user',
+                'placeholder':'user',
             }
         )
     )
@@ -24,10 +25,23 @@ class WebsitesForm(forms.ModelForm):
     password = forms.CharField(required=False, label='password',
             widget = forms.TextInput(attrs={
                 'class':'form-control', 
-                'placeholder': 'password',
+                'placeholder':'password',
             }
         )
     )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if not cleaned_data:
+            raise ValidationError("The Content is empty!")
+        
+        #validate if domain/hostname is not full/canonical url
+        domain = cleaned_data.get('domain')
+        
+        if not is_valid_hostname(domain):
+            self.add_error('domain', 'The domain is not valid.')
+        
+        return cleaned_data
 
     class Meta:
         model = Website
@@ -36,4 +50,3 @@ class WebsitesForm(forms.ModelForm):
             'user', 
             'password'
         ]
-        
